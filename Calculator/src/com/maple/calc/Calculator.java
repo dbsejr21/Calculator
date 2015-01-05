6cb7cceaf967c8ffeb313689d3751fd523a3afca
dbsejr21 <dbsejr21@gmail.com>
2015-01-05 18:04:06 +0900

    hexa 변환 메소드 추가 및 계산 내용 출력

--- Calculator/src/com/maple/calc/Calculator.java
+++ Calculator/src/com/maple/calc/Calculator.java
@@ -4,30 +4,42 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 3, b = 10;
+		System.out.println("a = " + Double.toString(a) + ", b = " + Double.toString(b) );
+		System.out.println("plus(a+b) : " + plus(a,b));
+		System.out.println("sub(a-b) : " + sub(a,b));
+		System.out.println("multiply(a*b) : " + multiply(a,b));
+		System.out.println("div(a/b) : " + div(a,b));
+		System.out.println("hexa(b) : " + hexa(b));
 	}
 	
-	String plus(int n1, int n2){
+	static String plus(double n1, double n2){
 		String retStr = null;
-		retStr = Integer.toString(n1 + n2);
+		retStr = Double.toString(n1 + n2);
 		return retStr;
 	}
 	
-	String sub(int n1, int n2){
+	static String sub(double n1, double n2){
 		String retStr = null;
-		retStr = Integer.toString(n1 - n2);
+		retStr = Double.toString(n1 - n2);
 		return retStr;
 	}
 	
-	String multiply(int n1, int n2){
+	static String multiply(double n1, double n2){
 		String retStr = null;
-		retStr = Integer.toString(n1 * n2);
+		retStr = Double.toString(n1 * n2);
 		return retStr;
 	}
 	
-	String div(int n1, int n2){
+	static String div(double n1, double n2){
 		String retStr = null;
-		retStr = Integer.toString(n1 / n2);
+		retStr = Double.toString(n1 / n2);
+		return retStr;
+	}
+	
+	static String hexa(int n){
+		String retStr = null;
+		retStr = Integer.toHexString(n);
 		return retStr;
 	}
 }
